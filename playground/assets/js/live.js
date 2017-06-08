8ed896e87c9d5fbcc07b9c0b1d97566686107247
chrome01 <yangaln1996@gmail.com>
2017-06-08 20:56:07 +0800

    mid problems fixed

--- playground/assets/js/live.js
+++ playground/assets/js/live.js
@@ -7,8 +7,12 @@ var father2child = {};
 //var socket = new WebSocket('ws://' + serverurl + "/lives/" + getlid() + "/thread");
 var uid = localStorage.uid;
 var hostid = 0;
-
-
+var targetid = 0;//随点击事件而更新，指定新消息的reply_to
+var temptargetid = 0;
+//考虑用户打开评论区后，评论完别的，还想针对这条主线消息评论，
+//为了防止该条主线消息mid丢失必须得到这个
+//关于上面两个id的测试我已经做过了
+var livepushlistener ;
 function init() {
 	$.ajax({
 		type: "GET",
@@ -35,7 +39,7 @@ Vue.component('message_item', {
 	methods: {
 		prepareReplylist: function(mid) {
 			replylist_provider.fillReplyList(mid);
-			localStorage.outmid = mid;
+			temptargetid = mid;
 		}
 	},
 	template: '\
@@ -57,6 +61,35 @@ Vue.component('message_item', {
 \
      '
 });
+Vue.component('message_item2', {
+	props: ["message"],
+	computed: {
+		istext: function() {
+			return this.message.content_type == "text";
+		}
+	},
+	methods: {
+		prepareReplylist: function(mid) {
+			replylist_provider.fillReplyList(mid);
+			temptargetid = mid;
+		}
+	},
+	template: '\
+     <div class="card facebook-card host-card">\
+          <div class="card-header no-border">\
+            <div class="facebook-avatar"><img :src="message.avatarimg" width="34" height="34"></div>\
+            <div class="facebook-name">{{message.hostname}}</div>\
+            <div class="facebook-date">{{message.time}}</div>\
+          </div>\
+          <div class="card-content">\
+            <div class="card-content-inner" v-if="istext">{{message.content}}</div>\
+              <div class = "card-content-inner" v-else><img :src="message.content"></img></div>\
+          </div>\
+        </div>\
+\
+\
+     '
+});
 Vue.component('reply_item', {
 	props: ["reply"],
 	computed: {
@@ -68,11 +101,12 @@ Vue.component('reply_item', {
 
 		popupWrite_sth: function(mid) {
 			$.popup("#write_sth");
+			targetid=mid;
 
 		},
 		popupPost_sth: function(mid) {
 			$.popup("#post_img");
-
+			targetid=mid;
 		}
 	},
 	template: '        <li>\
@@ -117,16 +151,18 @@ function addItem_general(items, list) {
 		})
 	}
 }
-var Hostbar = new Vue(
-{
-     el:"#hostbar",
-     computed:
-     {
-     	isHost:function()
-     	{
-     		return localStorage.hostid==localStorage.uid;
-     	}
-     }	
+var Hostbar = new Vue({
+	el: "#hostbar",
+	computed: {
+		isHost: function() {
+			return localStorage.hostid == localStorage.uid;
+		}
+	},
+	methods: {
+		setReplyMid: function() {
+			targetid = 0;
+		}
+	}
 })
 var message_list_provider = new Vue({
 	el: "#messagebox",
@@ -135,6 +171,11 @@ var message_list_provider = new Vue({
 		latest: 0
 	},
 	methods: {
+		initial: function() {
+			//reply_list.splice(0,reply_list.length);
+			message_list_provider.$data.message_list = [];
+
+		},
 		getAllList: function() {
 			this.$data.message_list = [];
 			additem_single({
@@ -188,6 +229,11 @@ var message_list_provider = new Vue({
 		},
 		addItem: function(items) {
 			addItem_general(items, message_list_provider.$data.message_list);
+		},
+		fillMessageList: function(mid) {
+			this.initial();
+			reply_tree_construct(mid, message_list_provider.$data.message_list);
+			this.addItem(father2child[mid]);
 		}
 	},
 	created: function() {
@@ -200,13 +246,14 @@ var replylist_provider = new Vue({
 	data: {
 		reply_list: [],
 		latest: 0,
-		nowmid: 0
+		
 	},
 	methods: {
 		initial: function() {
 			//reply_list.splice(0,reply_list.length);
-			replylist_provider.$data.reply_list=[];
-			latest = 0;
+			replylist_provider.$data.reply_list = [];
+
+
 		},
 		addItem: function(items) {
 
@@ -217,10 +264,10 @@ var replylist_provider = new Vue({
 			this.initial();
 			//alert(replylist_provider.$data.reply_list.length);
 			reply_tree_construct(mid, message_list_provider.$data.message_list);
-		
+
 			this.addItem(father2child[mid]);
 			//alert(replylist_provider.$data.reply_list.length);
-			return replylist_provider.$data.reply_list;
+			//return replylist_provider.$data.reply_list;
 		}
 	},
 	created: function() {
@@ -268,25 +315,20 @@ function getMessages() {
 
 }
 
-function AllMode() {
-	Mode = 0;
-	$(".visible_controll").show();
-}
 
-function HostOnlyMode() {
-	Mode = 1;
-	$(".visible_controll").hide();
-	replylist_provider.fillReplyList(0);
-}
 
-function postRawFile() {
+
+//这里的alert和头像设置的部分我就注释掉了...确认一下，pic.file.fid就是fid吧？
+//另外那个错误代码....能不能转成对应的错误信息？
+//注意：这里有所改动，因为需要发送图片，content的形式：{type:img,payload:url}
+function postRawFile(ReplytoMid) {
 	//this function does an HTTP POST to the remote URL with the raw content as the body
 	//file 		: 	File object, usually obtained in the way like $('#fileinput').files[0]
 	//settings 	: 	jQuery XHR settings object, refer to https://api.jquery.com/jquery.ajax/#jQuery-ajax-settings for more information.
 	//				Attention that this function overwrites type, contentType, data and processData in settings
 	var reader = new FileReader();
 	var files = $('input[name="file"]').prop('files');
-	alert(files[0].name);
+	//alert(files[0].name);
 	reader.onload = function() {
 		$.ajax({
 			type: 'POST',
@@ -298,17 +340,21 @@ function postRawFile() {
 			url: serverurl + "/files",
 			data: new Uint8Array(this.result),
 			success: function(data, status) {
-				alert(data);
+				//alert(data);
 				pic = JSON.parse(data);
-				alert(pic.file.fid);
+				//alert(pic.file.fid);     
 				var others = JSON.parse(localStorage.others)
 				$.ajax({
-					url: serverurl + "/users/" + localStorage.uid,
+					url: serverurl + "/lives/" + getlid() + "/thread",
 					type: 'POST',
 					data: JSON.stringify({
 						"message": {
 							"replyto": ReplyToMid,
-							"lid": getlid()
+							"lid": getlid(),
+							"content": JSON.stringify({
+								"type": "img",
+								"payload": serverurl + "/files/"+pic.file.fid
+							})
 						}
 
 					}),
@@ -316,8 +362,8 @@ function postRawFile() {
 						withCredentials: true
 					},
 					success: function(data, status) {
-						$("#avatar-panel").attr('src', serverurl + "/files/" + pic.file.fid);
-						$("#avatar-page").attr('src', serverurl + "/files/" + others.avatar);
+						//$("#avatar-panel").attr('src', serverurl + "/files/" + pic.file.fid);
+						//$("#avatar-page").attr('src', serverurl + "/files/" + others.avatar);
 					},
 					error: function(data, status) {
 						$.toast('发生了' + data.status + '错误');
@@ -331,14 +377,21 @@ function postRawFile() {
 	};
 	reader.readAsArrayBuffer(files[0]);
 }
-
+//只要postRawFile没错就没错
 function postImg() {
-	postRawFile();
+	// alert(temptargetid);
+	// alert(targetid);
+	postRawFile(targetid);
 	var file = $("#dmg")
 	file.after(file.clone().val(""));
 	file.remove();
 }
-
+function posttext(){
+	// alert(temptargetid);
+	// alert(targetid);
+	createMessage(targetid);
+}
+//已经测试
 function reply_tree_construct(mid, cache) {
 	var result = [];
 	child2father = {};
@@ -350,7 +403,7 @@ function reply_tree_construct(mid, cache) {
 		return a.time - b.time;
 	});
 	//tmp_cache=JSON.parse(json: string)
-	if (mid == -1) return cache;
+	if (mid == 0) return cache;
 	else {
 		$.each(cache, function(index, item) {
 
@@ -360,7 +413,7 @@ function reply_tree_construct(mid, cache) {
 			// alert("check"+(item.reply_to==mid));
 			if (item == undefined) return true;
 			if (item.reply_to == undefined || item.reply_to == null || item.reply_to == "" || item.reply_to == "0") return true;
-			if (item.reply_to == mid ) {
+			if (item.reply_to == mid) {
 				child2father[item.mid] = mid;
 				father2child[mid].push(item);
 			} else {
@@ -374,26 +427,31 @@ function reply_tree_construct(mid, cache) {
 		})
 	}
 	result = father2child[mid];
-	return result;
+	return result; //就是把某个mid的子孙message全部收集起来，
+	//函数返回的是整理好的array，嗯嗯就这样
 }
-
+//未测试，发送文字消息
 function createMessage(ReplyToMid) {
 	var message = $("#write_comment_content").val();
 	$.ajax({
-		url: serverurl + "/lives/" + getlid() + "/thread",
-		xhrFields: {
-			withCredentials: true
-		},
-		type: "POST",
-		data: JSON.stringify({
-			"content": JSON.stringify({
-				"type": "text",
-				"payload": message
+			url: serverurl + "/lives/" + getlid() + "/thread",
+			xhrFields: {
+				withCredentials: true
+			},
+			type: "POST",
+			data: JSON.stringify({
+				"content": JSON.stringify({
+					"type": "text",
+					"payload": message
+				}),
+				"replyto": ReplyToMid,
+				"lid": getlid()
+
+
 			}),
-			"replyto": ReplyToMid,
-			"lid": getlid()
-
-
-		}),
-	})
+		}) 
+}
+function setReplyMid_outer() //targetid设为temptargetid
+{
+	targetid = temptargetid;
 }
