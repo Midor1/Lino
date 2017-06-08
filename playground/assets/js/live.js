5e73899d104f4c2fc72c8138b548963fc22fdb9f
chrome01 <yangaln1996@gmail.com>
2017-06-09 00:20:43 +0800

    websocket imported,waiting for test

--- playground/assets/js/live.js
+++ playground/assets/js/live.js
@@ -31,7 +31,6 @@ function init() {
 			localStorage.hostid = hostid;
 		}
 	});
-	livepushlistener = new LivePushListener("ws://q.aureliano.cc:4567/websocket/lives",getlid(),message_list_provider.addItem);
 }
 
 Vue.component('message_item', {
@@ -458,6 +457,57 @@ function setReplyMid_outer() //targetid设为temptargetid
 {
 	targetid = temptargetid;
 }
+function LiveMessage(naiveMessage) {
+	this.mid = naiveMessage.mid;
+	this.owner = naiveMessage.owner;
+	this.reply_to = naiveMessage.replyto;
+	this.content = naiveMessage.conent.payload;
+	this.content_type = naiveMessage.content.type;
+	this.time = naiveMessage.time;
+}
 
+
+
+function LivePushListener(baseurl, lid, onMessage) {
+	//baseurl looks like 'www.lino.com/websocket/lives' or '/websocket/lives' depending on the demend. 
+	this.lid = lid;
+	this.onMessage = onMessage;
+	this.baseurl = baseurl;
+	this.url = baseurl + '?lid=' + lid;
+
+	this.retry_time = 500;
+
+	this.ws_onerror = function ws_onerror() {
+		this.retry_time = this.retry_time * 2;
+		console.error('Failed to connect to WebSocket server, retrying in ' + this.retry_time / 1000 + ' seconds');
+		setTimeout(this.wsconnect, this.retry_time);
+	}
+
+	this.ws_onclose = function ws_onclose() {
+		this.retry_time = this.retry_time * 2;
+		console.error('Disconnected WebSocket server, retrying in ' + this.retry_time / 1000 + ' seconds');
+		setTimeout(this.wsconnect, this.retry_time);
+	}
+
+	this.ws_onmessage = function ws_onmessage(e) {
+		this.onMessage(new LiveMessage(JSON.parse(e.data)));
+	}
+
+	this.ws_onopen = function ws_onopen() {
+		this.retry_time = 500;
+	}
+
+	this.wsconnect = function wsconnect() {
+		this.websocket =new WebSocket(this.url);
+		this.websocket.onclose = this.ws_onclose;
+		this.websocket.onerror = this.ws_onerror;
+		this.websocket.onmessage = this.ws_onmessage;
+	}
+
+	this.wsconnect();
+
+}
 if (localStorage.hostid !== localStorage.uid)
 	$("#hostbar").remove();
+livepushlistener = new LivePushListener("ws://q.aureliano.cc:4567/websocket/lives",getlid(),message_list_provider.addItem);
+
